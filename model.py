ace1d20eb3f0a250eff3c784484a609f1a93dcf1
jyeongho <40686772+jyeongho@users.noreply.github.com>
2021-11-17 22:40:27 +0900

    Add files via upload

---
+++ model.py
@@ -0,0 +1,135 @@
+import torch
+from torch import nn
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, in_channels = 3, patch_size = 16, emb_size = 768, img_size = 224):
+        super(PatchEmbedding, self).__init__()
+        self.projection = nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size)
+        self.cls_token = nn.Parameter(torch.randn(1,1, emb_size))
+        self.positions = nn.Parameter(torch.randn((img_size // patch_size) **2 + 1, emb_size))
+        
+    def forward(self, x):
+        b, _, _, _ = x.shape    # b x c x (h x p) x (w x p)
+        x = self.projection(x)  # b x e x h x w
+        x = x.flatten(2)    # b x e x (h x w)
+        x = x.transpose(1, 2)   # b x (h x w) x e
+        cls_tokens = self.cls_token.expand(b, -1, -1)   # b x 1 x e
+        x = torch.cat([cls_tokens, x], dim=1)   # b x (h x w + 1) x e
+        x += self.positions
+        return x
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, emb_size = 768, num_heads = 12):
+        super(MultiHeadAttention, self).__init__()
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+        self.head_size = int(self.emb_size / self.num_heads)
+        self.keys = nn.Linear(emb_size, emb_size)
+        self.queries = nn.Linear(emb_size, emb_size)
+        self.values = nn.Linear(emb_size, emb_size)
+        self.projection = nn.Linear(emb_size, emb_size)
+        self.scaling = self.head_size ** -0.5
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        b, n, _ = x.shape
+        queries = self.queries(x).view(b, n, self.num_heads, self.head_size).permute(0, 2, 1, 3)
+        keys = self.keys(x).view(b, n, self.num_heads, self.head_size).permute(0, 2, 1, 3)
+        values  = self.values(x).view(b, n, self.num_heads, self.head_size).permute(0, 2, 1, 3)
+
+        attention_scores = torch.matmul(queries, keys.transpose(-1, -2))  # bhqd x bhdk -> bhqk
+        attention_scores = attention_scores * self.scaling
+        attention_probs = self.softmax(attention_scores)
+        
+        out = torch.matmul(attention_probs, values).permute(0, 2, 1, 3).contiguous().view(b, n, self.emb_size)
+        out = self.projection(out)
+        return out
+
+
+class FeedForwardBlock(nn.Module):
+    def __init__(self, emb_size, expansion = 4, dropout = 0.1):
+        super(FeedForwardBlock, self).__init__()
+        mlp_size = expansion * emb_size
+        self.mlp = nn.Sequential(
+            nn.Linear(emb_size, mlp_size),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(mlp_size, emb_size),
+            nn.Dropout(dropout)
+        )
+    
+    def forward(self, x):
+        out = self.mlp(x)
+
+        return x
+
+
+
+class TransformerEncoderBlock(nn.Module):
+    def __init__(self, emb_size = 768, num_heads = 12, dropout = 0.1, expansion = 4):
+        super(TransformerEncoderBlock, self).__init__()
+        
+        self.attention_norm = nn.LayerNorm(emb_size)
+        self.mlp_norm = nn.LayerNorm(emb_size)
+        self.attention = MultiHeadAttention(emb_size, num_heads)
+        self.mlp = FeedForwardBlock(emb_size, expansion, dropout)
+
+        self.attn_dropout = nn.Dropout(dropout)
+        
+
+    def forward(self, x):
+        h = x
+        x = self.attention_norm(x)
+        x = self.attention(x)
+        x = self.attn_dropout(x)
+        x = x + h
+
+        h = x
+        x = self.mlp_norm(x)
+        x = self.mlp(x)
+        x = x + h
+        return x
+
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, layers = 12, emb_size = 768, num_heads = 12, dropout = 0.1, expansion = 4):
+        super(TransformerEncoder, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.Encoders = nn.Sequential(*[TransformerEncoderBlock(emb_size, num_heads, dropout, expansion) for _ in range(layers)])
+
+    def forward(self, x):
+        out = self.Encoders(x)
+
+        return out
+
+
+class ClassificationHead(nn.Module):
+    def __init__(self, emb_size = 768, n_classes = 1000):
+        super(ClassificationHead, self).__init__()
+        
+        self.layernorm = nn.LayerNorm(emb_size)
+        self.mlp_head = nn.Linear(emb_size, n_classes)
+
+    def forward(self, x):
+        x = x[:, 0]      # b x n x e -> b x e
+        out = self.mlp_head(self.layernorm(x))
+
+        return out
+
+class ViT(nn.Module):
+    def __init__(self, in_channels = 3, patch_size = 16, emb_size = 768, img_size = 224, layers = 12, n_classes = 1000, num_heads = 12, dropout = 0.1, expansion = 4):
+        super(ViT, self).__init__()
+        self.n_classes = n_classes
+        self.patchembedding = PatchEmbedding(in_channels, patch_size, emb_size, img_size)
+        self.transformerencoder = TransformerEncoder(layers, emb_size, num_heads, dropout, expansion)
+        self.classificationhead = ClassificationHead(emb_size, n_classes)
+
+    def forward(self, x):
+        out = self.patchembedding(x)
+        out = self.transformerencoder(out)
+        out = self.classificationhead(out).view(-1, self.n_classes)
+        
+        return out
